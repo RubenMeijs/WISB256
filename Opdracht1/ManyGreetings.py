ab6d675f08b1ceef848b619235553714b3aeaee6
rubenmeijs <rubenmeijs@gmail.com>
2015-04-21 10:16:07 +0000

    python 3.4 ipv 2.7

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,4 +1,4 @@
-name =  raw_input("What is your name? ")
-greetings =  raw_input("How many greetings? ")
+name =  input("What is your name? ")
+greetings =  input("How many greetings? ")
 
 print(greetings + " x Hello, " + name + "!")
