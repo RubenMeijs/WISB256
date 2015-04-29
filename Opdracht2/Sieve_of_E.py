eec325e7a32226ab37fcb17156c64d53fdcbc1ba
rubenmeijs <rubenmeijs@gmail.com>
2015-04-29 08:34:53 +0000

    extension sieve of e

---
+++ Opdracht2/Sieve_of_E.py
@@ -0,0 +1,26 @@
+import sys
+import timeit
+import math
+
+N = int(sys.argv[1])
+output = str(sys.argv[2])
+
+T1=timeit.default_timer()
+def sieve(a):
+    wortel=int(math.ceil((math.sqrt(a))))
+    lijst1 = list(range(2,a))
+    minilijst=list(range(2,wortel))
+    for mini in minilijst:
+        for getal in lijst1:
+            if getal%mini==0 and getal != mini:
+                lijst1.remove(getal)
+    return(lijst1)
+
+prime = open(output,'w')
+data = sieve(N)
+for i in data:
+    prime.write(str(i) + "\n")
+    
+T2=timeit.default_timer()
+scherm='Found ' + str(len(data)) + ' Prime numbers smaller than ' + str(N) +  ' in ' +  str(T2-T1) + ' sec'
+print(scherm)
