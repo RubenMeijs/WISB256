eb3590f40e11c1eac24adf89b9f81a46341df521
rubenmeijs <rubenmeijs@gmail.com>
2015-04-29 09:04:40 +0000

    sieve of e versnelt

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -8,8 +8,9 @@ output = str(sys.argv[2])
 T1=timeit.default_timer()
 def sieve(a):
     wortel=int(math.ceil((math.sqrt(a))))
-    lijst1 = list(range(2,a))
-    minilijst=list(range(2,wortel))
+    lijst1 = list(range(3,a,2))
+    lijst1.append(2)
+    minilijst=list(range(3,wortel))
     for mini in minilijst:
         for getal in lijst1:
             if getal%mini==0 and getal != mini:
