e3739761f16e24a0fcfb80f8b5abdf6e0f97e93c
rubenmeijs <rubenmeijs@gmail.com>
2015-05-02 18:47:47 +0000

    sneller :)

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -7,16 +7,22 @@ output = str(sys.argv[2])
 
 T1=timeit.default_timer()
 def sieve(a):
+    
     wortel=int(math.ceil((math.sqrt(a))))
     lijst1 = list(range(3,a,2))
     lijst1.append(2)
     lijst1.sort()
     minilijst=list(range(3,wortel))
+    
     for mini in minilijst:
         for getal in lijst1:
             if getal%mini==0 and getal != mini:
-                getal=0
+                lijst1[(getal-1)/2]=0
+    lijst1=list(set(lijst1))
+    lijst1.remove(0)
     return(lijst1)
+
+
 prime = open(output,'w')
 data = sieve(N)
 for i in data:
