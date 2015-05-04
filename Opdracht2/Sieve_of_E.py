332b78896283caaa0efaf92dd216bf047f5727b3
rubenmeijs <rubenmeijs@gmail.com>
2015-05-04 18:52:09 +0000

    minder dan 2 sec

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -10,16 +10,16 @@ def sieve(a):
     
     wortel=int(math.ceil((math.sqrt(a))))
     lijst1 = list(range(3,a,2))
-    lijst1.append(2)
-    lijst1.sort()
-    minilijst=list(range(3,wortel))
+    minilijst=list(range(3,wortel,2))
     
     for mini in minilijst:
         for getal in lijst1:
             if getal%mini==0 and getal != mini:
-                lijst1[(getal-1)/2]=0
+                lijst1[(getal-1)/2-1]=0
     lijst1=list(set(lijst1))
     lijst1.remove(0)
+    lijst1.append(2)
+    lijst1.sort()
     return(lijst1)
 
 
